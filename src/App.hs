168cebcaaa367973e60f49a1d712a778511be269
Peter Murphy <pete@simspace.com>
2020-06-20 15:38:18 -0400

    Initial commit

---
+++ src/App.hs
@@ -0,0 +1,38 @@
+module App
+  ( postReservation,
+  )
+where
+
+import ApiModel
+import Control.Monad (forM_)
+import Control.Monad.Trans (liftIO)
+import Control.Monad.Trans.Either
+  ( EitherT (..),
+    eitherT,
+    hoistEither,
+    right,
+  )
+import DB
+
+connStr :: ConnectionString
+connStr = "."
+
+svcAddr :: ServiceAddress
+svcAddr = "."
+
+checkCaravan :: Reservation -> Error -> EitherT Error IO Reservation
+checkCaravan reservation err = do
+  c <- liftIO $ findCaravan svcAddr (quantity reservation) (date reservation)
+  newRes <- hoistEither $ checkCaravanCapacityOnError err c reservation
+  liftIO $ forM_ c $ reserveCaravan svcAddr (date newRes)
+  return newRes
+
+postReservation :: ReservationRendition -> IO (HttpResult ())
+postReservation candidate =
+  fmap toHttpResult
+    $ runEitherT
+    $ do
+      r <- hoistEither $ validateReservation candidate
+      i <- liftIO $ getReservedSeatsFromDB connStr $ date r
+      eitherT (checkCaravan r) right $ hoistEither $ checkCapacity 10 i r
+      >>= liftIO . saveReservation connStr
